51aa04edbfe045f50e9b1c45879a9f23ae87da0a
Luis Caceres <luiscaceresmx@gmail.com>
2021-07-11 19:30:59 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ function onYouTubeIframeAPIReady() {
   player.addEventListener('onReady', onReady);
 }
 
-async function onReady() {
-  console.log('Hola mundo!');
+async function onReady(event) {
+  await verifyAvailability(event);
+}
+
+async function verifyAvailability({target}) {
+  const player = target;
+  const videos = [add, intro].concat(musicVideos, pool);
+
+  function verifier(resolve, video) {
+    // Notify if this music video cannot be played.
+    player.getPlayerState() !== 1 && console.log(video);
+    // Move on to the next music video.
+    resolve();
+  }
+
+  let promise;
+
+  for (const video of videos) {
+    // Attempt to play this video.
+    player.loadVideoById(video);
+    // Wait 5 seconds and verify if this video has loaded and is playing.
+    promise = await new Promise(resolve => setTimeout(verifier, 5000, resolve, video));
+  }
+
+  return promise;
 }
