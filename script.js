cda2246402fdc74ed42b0bb64258a69893f0ab5f
Luis Caceres <luiscaceresmx@gmail.com>
2021-07-11 19:15:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const script = document.createElement('script');
+script.src = "https://www.youtube.com/iframe_api";
+document.body.append(script);
+
 function onYouTubeIframeAPIReady() {
   const player = new YT.Player('player');
   player.addEventListener('onReady', onReady);
