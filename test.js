4143d382bb45ee714cd927c8b744263b55790cab
Luis Caceres <luiscaceresmx@gmail.com>
2021-07-12 23:22:25 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
+const expect = chai.expect;
 
+// encode()
+{
+  const expectation = 'Ifmmp!xpsme\"'
+  const value = encode('Hello world!');
+  expect(value).to.equal(expectation);
+}
+
+// random()
+{
+  const value = random(0);
+  const expectation = 0;
+  expect(value).to.equal(expectation);
+  expect(value).to.equal(10);
+}
