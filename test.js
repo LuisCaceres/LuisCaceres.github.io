2ab1a39cc1a1fb5ad5c59193fd1c37201ae9fb0c
Luis Caceres <luiscaceresmx@gmail.com>
2021-07-13 20:26:03 +1000

    Update test.js

--- test.js
+++ test.js
@@ -7,25 +7,67 @@ const expect = chai.expect;
     ['Artist 3', 'Title 3'],
   ];
   
-  const videos = [
+  const charted = [
     { artist: 'Artist 1', title: 'Title 1', match: 'Title 1' },
     { artist: 'Artist 2', title: 'Title 2', match: 'Title 2' },
   ];
   
-  const pool = [
+  const uncharted = [
     { artist: 'Artist 3', title: 'Title 3' },
   ];
   
-  const value = associate(items, videos, pool);
+  const value = associate(items, charted, uncharted);
   
-  expect(value.items.length).to.equal(2);
-  expect(value.items[0]).to.equal(videos[0]);  // Artist 1
-  expect(value.items[1]).to.equal(pool[0]);    // Artist 3
-  expect(value.items[1].hasOwnProperty('match')).to.equal(true);
+  expect(value.chart.length).to.equal(2);
+  expect(value.chart[0]).to.equal(videos[0]);  // Artist 1
+  expect(value.chart[1]).to.equal(pool[0]);    // Artist 3
+  expect(value.chart[1].hasOwnProperty('match')).to.equal(true);
   
   expect(value.pool.length).to.equal(0);
   expect(value.videos.length).to.equal(3);
 }
+
+
+// generatePlaylist()
+//
+{
+  const entries = [
+    {artist: 'Artist 20', title: 'Title 20' },
+    {artist: 'Artist 19', title: 'Title 19' },
+    {artist: 'Artist 18', title: 'Title 18' },
+    {artist: 'Artist 17', title: 'Title 17' },
+    {artist: 'Artist 16', title: 'Title 16' },
+    {artist: 'Artist 15', title: 'Title 15' },
+    {artist: 'Artist 14', title: 'Title 14' },
+    {artist: 'Artist 13', title: 'Title 13' },
+    {artist: 'Artist 12', title: 'Title 12' },
+    {artist: 'Artist 11', title: 'Title 11' },
+    {artist: 'Artist 10', title: 'Title 10' },
+    {artist: 'Artist 09', title: 'Title 09' },
+    {artist: 'Artist 08', title: 'Title 08' },
+    {artist: 'Artist 07', title: 'Title 07' },
+    {artist: 'Artist 06', title: 'Title 06' },
+    {artist: 'Artist 05', title: 'Title 05' },
+    {artist: 'Artist 04', title: 'Title 04' },
+    {artist: 'Artist 03', title: 'Title 03' },
+    {artist: 'Artist 02', title: 'Title 02' },
+    {artist: 'Artist 01', title: 'Title 01' },
+  ];
+     
+  const playlist = generatePlaylist(entries);
+  
+  expect(playlist.length).to.equal();
+  expect(playlist[0]).to.equal(intro);      // curtain
+  expect(playlist[1]).to.equal(entries[0]); // position 20
+  expect(playlist[2]).to.equal();
+  expect(playlist[3]).to.equal();
+  expect(playlist[4]).to.equal();
+  expect(playlist[5]).to.equal();
+  expect(playlist[6]).to.equal();
+  expect(playlist[7]).to.equal();
+  expect(playlist[8]).to.equal();
+}
+
 
 // encode()
 // encode a string
