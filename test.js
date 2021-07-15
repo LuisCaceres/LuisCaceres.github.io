10995feceef8efb5dffb46f315993f5bd7f8cc81
Luis Caceres <luiscaceresmx@gmail.com>
2021-07-15 20:30:25 +1000

    Update test.js

--- test.js
+++ test.js
@@ -7,10 +7,7 @@ const expect = chai.expect;
 // associate()
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
-  const list = [
-    ['Artist 1', 'Title 1'],
-    ['Artist 3', 'Title 3'],
-  ];
+  const list = [Title 1', 'Title 2'];
   
   const charted = [
     { artist: 'Artist 1', title: 'Title 1', match: 'Title 1' },
