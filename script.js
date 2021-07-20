7695ec89217f3499a74a14e662ba8398fed03930
Luis Caceres <luiscaceresmx@gmail.com>
2021-07-21 02:40:26 +1000

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function adjustScreen(screen, measurements) {
  */
 function adjustPlaylist(playlist) {
   return playlist.map(item => {
-    if (item.hasProperty('endSeconds') {
+    if (item.hasProperty('endSeconds')) {
       return [item, item];
     }
   }).flat();
