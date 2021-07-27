d8ad6fb281e39307e0589e0238ea68fbdb722b0f
Luis Caceres <luiscaceresmx@gmail.com>
2021-07-27 23:33:33 +1000

    Update test.js

--- test.js
+++ test.js
@@ -64,33 +64,33 @@ function generateList(outcoming, incoming) {
 // generatePlaylist()
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
-  const chart = new List(
-    '20',
-    '19',
-    '18',
-    'xtra', // VIDEO EXTRA
-    '17',
-    '16',
-    '15',
-    '14',
-    '13',
-    '12',
-    '11',
-    '10',
-    '09',
-    '08',
-    'xtra', // VIDEO EXTRA
-    '07',
-    '06',
-    '05',
-    '04',
-    '03',
-    '02',
-    '01',
-  );
+//   const chart = new List(
+//     '20',
+//     '19',
+//     '18',
+//     'xtra', // VIDEO EXTRA
+//     '17',
+//     '16',
+//     '15',
+//     '14',
+//     '13',
+//     '12',
+//     '11',
+//     '10',
+//     '09',
+//     '08',
+//     'xtra', // VIDEO EXTRA
+//     '07',
+//     '06',
+//     '05',
+//     '04',
+//     '03',
+//     '02',
+//     '01',
+//   );
      
-  const playlist = generatePlaylist(chart, 'INTRO', 'STING', 'ADVERTISEMENT', 'STING NEW VIDEO');
-  validate(playlist);
+//   const playlist = generatePlaylist(chart, 'INTRO', 'STING', 'ADVERTISEMENT', 'STING NEW VIDEO');
+//   validate(playlist);
 }
 
 
