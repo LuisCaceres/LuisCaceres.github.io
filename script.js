7aafa747f31609974f2d81e80bc6b4fd20dd3858
Luis Caceres <luiscaceresmx@gmail.com>
2021-08-19 00:15:43 +1000

    Update script.js

--- script.js
+++ script.js
@@ -224,7 +224,7 @@ class List extends Array {
    *
    */
   share(...lists) {
-    const value = lists.concat(this).map((list, index) => {
+    const value = [this].concat(lists).map((list, index) => {
       
       if (list.length === 0) {
         return null;
