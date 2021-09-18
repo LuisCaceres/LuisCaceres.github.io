1806b9b3b4292ba2bc4f58edd30b19130bfe6582
Luis Caceres <luiscaceresmx@gmail.com>
2021-09-18 22:43:29 +1000

    Update foo.js

--- foo.js
+++ foo.js
@@ -82,60 +82,63 @@ function createTable(...charts) {
  * createDatabase(chart1, chart2);
  * // Returns {'A' => { history: [1, 5])}, 'B' => { history: [2, 4])}, 'C' => { history: [3, 3])}, 'D' => { history: [4, 2])}, 'E' => { history: [5, 1])}}
  */
-// function runTests(week, charts, tests) {
+function runTests(week, charts, tests) {
 
-//   for (let index = 0; index < 20; index++) {
-//     const group = charts.map(chart => chart.slice());
-//     const [chart1, chart2, chartA, chartB] = group.slice(week - 3, week + 1);
+  for (let index = 0; index < 20; index++) {
+    const group = charts.map(chart => chart.slice());
+    const [chart1, chart2, chartA, chartB] = group.slice(week - 3, week + 1);
     
-//     const entry = chartA.at(index + 1);
-//     chart1.move(entry, index);
-//     chart2.move(entry, index);
+    const entry = chartA.at(index + 1);
+    chart1.move(entry, index);
+    chart2.move(entry, index);
 
-//     const database = createDatabase(...group.slice(0, week - 1));
-//     const entries = Chart.detector3(chartA, chartB, database);
+    const database = createDatabase(...group.slice(0, week - 1));
+    const entries = Chart.detector3(chartA, chartB, database);
+    
+    createTable(chart1, chart2, chartA, chartB);
+    debugger;
 
-//     tests[index].forEach((test, index) => {
+    tests[index].forEach((test, index) => {
 
-//       if (index === 0) {
-//         test(entries);
-//       }
-//       else {
-//         test(entries[index]);
-//       }
-//     });
-//   }
-// }
+      if (index === 0) {
+        test(entries);
+      }
+      else {
+        test(entries[index]);
+      }
+    });
+  }
+}
 
-// // WEEK 9
-// {
-//   const tests = [
-//     // POSITION 1
-//     [
-//       function (entries) {
-//        expect(entries.length).to.equal(1);
-//        expect(entries).to.include('I Need To Know');
-//       },
-//       function (entry) {
-//         expect(value.length).to.equal(1);                   // I Need To Know
-//         expect(value).to.include('That\'s The Way It Is');  // [04, 04, 06, 06] [05, 05, 04, 09]
-//       },
-//     ],
-//     // POSITION 2
-//     [ 
-//       function (entries) {
-//        expect(entries.length).to.equal(1);
-//        expect(entries).to.include('I Need To Know');
-//       },
-//       function (entry) {
-//         expect(value.length).to.equal(1);                   // I Need To Know
-//         expect(value).to.include('That\'s The Way It Is');  // [04, 04, 06, 06] [05, 05, 04, 09]
-//       },
-//     ],
-//   ];
+// WEEK 9
+{
+  const tests = [
+    // POSITION 1
+    [
+      function (entries) {
+       expect(entries.length).to.equal(1);
+       expect(entries).to.include('I Need To Know');
+      },
+      function (entry) {
+        expect(value.length).to.equal(1);                   // I Need To Know
+        expect(value).to.include('That\'s The Way It Is');  // [04, 04, 06, 06] [05, 05, 04, 09]
+      },
+    ],
+    // POSITION 2
+    [ 
+      function (entries) {
+       expect(entries.length).to.equal(1);
+       expect(entries).to.include('I Need To Know');
+      },
+      function (entry) {
+        expect(value.length).to.equal(1);                   // I Need To Know
+        expect(value).to.include('That\'s The Way It Is');  // [04, 04, 06, 06] [05, 05, 04, 09]
+      },
+    ],
+  ];
 
-//   runTests(9, charts);  
-// }
+  runTests(9, charts);  
+}
   
-// // WEEK 10 - IMPOSSIBLE TO TEST WEEK 10 BECAUSE WEEK 11 IS UNAVAILABLE.
+// WEEK 10 - IMPOSSIBLE TO TEST WEEK 10 BECAUSE WEEK 11 IS UNAVAILABLE.
 
