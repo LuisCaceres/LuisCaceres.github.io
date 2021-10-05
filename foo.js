99ca26b772a870cf0783dc46b5702dc83461ae7e
Luis Caceres <luiscaceresmx@gmail.com>
2021-10-05 15:27:08 +1100

    Update foo.js

--- foo.js
+++ foo.js
@@ -170,6 +170,33 @@ function runTests(week, charts, tests) {
        expect(entries).to.include("Someday");      // [09, 09, 10, 11]  [06, 07, 09, 13]
      },
    ],
+    
+   // POSITION 3
+   [
+     new Chart("All Star", "I Need To Know", "Mi Chico Latino", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Higher", "That's The Way It Is", "Man! I Feel Like A Woman", "If Ya Gettin' Down", "Don't Say You Love Me", "Dreams In Digital", "The Kids Aren't Alright", "When You're Gone", "As Fast As You Can"),
+     new Chart("All Star", "Angels", "Mi Chico Latino", "I Need To Know", "Someday We'll Know", "New", "Someday", "That's The Way It Is", "Si Me Advertí", "Puente", "No Quiero Verte", "Man! I Feel Like A Woman", "Don't Say You Love Me", "All I Have To Give", "Dreams In Digital", "All Or Nothing", "Higher", "As Fast As You Can", "If Ya Gettin' Down", "When You're Gone"),
+
+     function (entries) {
+       expect(entries.length).to.equal(2);
+       expect(entries).to.include("Mi Chico Latino");
+       expect(entries).to.include("Si Me Advertí");
+     },
+
+     function (entries) { // Mi Chico Latino
+       expect(entries.length).to.equal(1);
+                                              // [03, 03, 03, 02]  [04, 02, 02, 03]
+       expect(entries).to.include("Angels");  // [03, 03, 02, 02]  [04, 02, 03, 03]
+     },
+
+     function (entries) { // Si Me Advertí
+       expect(entries.length).to.equal(2);
+                                                   // [09, 09, 09, 11]  [**, **, 08, 08]
+       expect(entries).to.include("Maria Maria");  // [09, 09, 08, 11]  [**, **, 09, 08]
+       
+                                                   // [09, 09, 09, 11]  [06, 07, 10, 13]
+       expect(entries).to.include("Someday");      // [09, 09, 10, 11]  [06, 07, 09, 13]
+     },
+   ],
   ];
 
   runTests(7, charts, tests);
