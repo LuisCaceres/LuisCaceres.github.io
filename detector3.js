d7bf2490f1d37c39c99361565a6ef0b30dcd69e4
Luis Caceres <luiscaceresmx@gmail.com>
2021-10-06 01:01:47 +1100

    Update detector3.js

--- detector3.js
+++ detector3.js
@@ -239,6 +239,36 @@
        expect(entries).to.include("Maria Maria");    // [11, 11, 08, 07]  [**, **, 11, 08]
      },
    ],
+
+   // POSITION 12
+   [
+     new Chart("All Star", "I Need To Know", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Mi Chico Latino", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Man! I Feel Like A Woman", "Higher", "That's The Way It Is", "If Ya Gettin' Down", "Don't Say You Love Me", "Dreams In Digital", "The Kids Aren't Alright", "When You're Gone", "As Fast As You Can"),
+     new Chart("All Star", "Angels", "I Need To Know", "Mi Chico Latino", "Someday We'll Know", "New", "Someday", "That's The Way It Is", "Si Me Advertí", "Puente", "No Quiero Verte", "Man! I Feel Like A Woman", "Don't Say You Love Me", "All I Have To Give", "Dreams In Digital", "All Or Nothing", "Higher", "As Fast As You Can", "If Ya Gettin' Down", "When You're Gone"),
+
+     function (entries) {
+       expect(entries.length).to.equal(2);
+       expect(entries).to.include("Si Me Advertí");
+       expect(entries).to.include("Man! I Feel Like A Woman");
+     },
+
+     function (entries) { // Si Me Advertí
+       expect(entries.length).to.equal(2);
+                                                   // [09, 09, 09, 11]  [**, **, 08, 08]
+       expect(entries).to.include("Maria Maria");  // [09, 09, 08, 11]  [**, **, 09, 08]
+
+                                                   // [09, 09, 09, 11]  [06, 07, 10, 13]
+       expect(entries).to.include("Someday");      // [09, 09, 10, 11]  [06, 07, 09, 13]
+     },
+
+     function (entries) { // Man! I Feel Like A Woman
+       expect(entries.length).to.equal(2);
+                                                      // [12, 12, 12, 18]  [05, 10, 13, 16]
+       expect(entries).to.include("Puente");          // [12, 12, 13, 18]  [05, 10, 12, 16]
+
+                                                      // [12, 12, 12, 18]  [**, 16, 14, 10]
+       expect(entries).to.include("All Or Nothing");  // [12, 12, 14, 18]  [**, 16, 12, 10]
+     },
+   ],
   ];
 
   runTests(9, charts, 3, tests);
