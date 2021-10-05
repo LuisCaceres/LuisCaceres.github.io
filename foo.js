07adb037b340b867ba7a782de02cf4e9824b411c
Luis Caceres <luiscaceresmx@gmail.com>
2021-10-05 16:47:21 +1100

    Update foo.js

--- foo.js
+++ foo.js
@@ -279,6 +279,16 @@ function runTests(week, charts, tests) {
        expect(entries).to.include("Someday");      // [09, 09, 10, 11]  [06, 06, 09, 13]
      },
    ],
+
+   // POSITION 8
+   [
+     new Chart("All Star", "I Need To Know", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Maria Maria", "Mi Chico Latino", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Higher", "That's The Way It Is", "Man! I Feel Like A Woman", "If Ya Gettin' Down", "Don't Say You Love Me", "Dreams In Digital", "The Kids Aren't Alright", "As Fast As You Can"),
+     new Chart("All Star", "Angels", "I Need To Know", "Mi Chico Latino", "Someday We'll Know", "New", "Someday", "Maria Maria", "Si Me Advertí", "That's The Way It Is", "Puente", "No Quiero Verte", "Man! I Feel Like A Woman", "Don't Say You Love Me", "All I Have To Give", "Dreams In Digital", "All Or Nothing", "Higher", "As Fast As You Can", "If Ya Gettin' Down"),
+
+     function (entries) {
+       expect(entries.length).to.equal(0);
+     },
+   ],
   ];
 
   runTests(7, charts, tests);
