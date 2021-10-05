227043109049b9d0773f811a75cf83cbf3dccd86
Luis Caceres <luiscaceresmx@gmail.com>
2021-10-05 14:46:02 +1100

    Update foo.js

--- foo.js
+++ foo.js
@@ -121,6 +121,30 @@ function runTests(week, charts, tests) {
 }
 
 
+// WEEK 7
+{
+  const tests = [
+   // POSITION 1
+   [
+     new Chart("All Star", "I Need To Know", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Mi Chico Latino", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Higher", "That's The Way It Is", "Man! I Feel Like A Woman", "If Ya Gettin' Down", "Don't Say You Love Me", "Dreams In Digital", "The Kids Aren't Alright", "When You're Gone", "As Fast As You Can"),
+     new Chart("All Star", "Angels", "I Need To Know", "Mi Chico Latino", "Someday We'll Know", "New", "Someday", "That's The Way It Is", "Si Me Advertí", "Puente", "No Quiero Verte", "Man! I Feel Like A Woman", "Don't Say You Love Me", "All I Have To Give", "Dreams In Digital", "All Or Nothing", "Higher", "As Fast As You Can", "If Ya Gettin' Down", "When You're Gone"),
+
+     function (entries) {
+       expect(entries.length).to.equal(1);
+       expect(entries).to.include("Si Me Advertí");
+     },
+
+     function (entries) { // Si Me Advertí
+       expect(entries.length).to.equal(1);
+                                               // [09, 09, 09, 11]  [06, 07, 10, 13]
+       expect(entries).to.include("Someday");  // [09, 09, 10, 11]  [06, 07, 09, 13]
+     },
+   ],
+  ];
+
+  runTests(7, charts, tests);
+}
+    
 // WEEK 8
 {
   const tests = [
