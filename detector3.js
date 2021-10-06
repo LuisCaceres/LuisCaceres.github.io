f0a94cef12637f7d266b68dbd6b5c5ea145470fa
Luis Caceres <luiscaceresmx@gmail.com>
2021-10-06 15:22:07 +1100

    Update detector3.js

--- detector3.js
+++ detector3.js
@@ -470,7 +470,25 @@
      },
    ],
 
-// AAAAA
+   // POSITION 20
+   [
+     new Chart("All Star", "I Need To Know", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Mi Chico Latino", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Higher", "That's The Way It Is", "Man! I Feel Like A Woman", "If Ya Gettin' Down", "Don't Say You Love Me", "The Kids Aren't Alright", "When You're Gone", "As Fast As You Can", "Dreams In Digital"),
+     new Chart("All Star", "Angels", "I Need To Know", "Mi Chico Latino", "Someday We'll Know", "New", "Someday", "That's The Way It Is", "Si Me Advertí", "Puente", "No Quiero Verte", "Man! I Feel Like A Woman", "Don't Say You Love Me", "All I Have To Give", "All Or Nothing", "Higher", "As Fast As You Can", "If Ya Gettin' Down", "When You're Gone", "Dreams In Digital"),
+
+     function (entries) {
+       expect(entries.length).to.equal(1);
+       expect(entries).to.include("Si Me Advertí");
+     },
+
+     function (entries) { // Si Me Advertí
+       expect(entries.length).to.equal(2);
+                                                   // [09, 09, 09, 11]  [**, **, 08, 08]
+       expect(entries).to.include("Maria Maria");  // [09, 09, 08, 11]  [**, **, 09, 08]
+
+                                                   // [09, 09, 09, 11]  [06, 07, 10, 13]
+       expect(entries).to.include("Someday");      // [09, 09, 10, 11]  [06, 07, 09, 13]
+     },
+   ],
   ];
 
   runTests(9, charts, 3, tests);
