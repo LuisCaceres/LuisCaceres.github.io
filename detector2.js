4f273cc9a3bd71b2947118d614813393d38f8be1
Luis Caceres <luiscaceresmx@gmail.com>
2021-10-14 20:39:14 +1100

    Update detector2.js

--- detector2.js
+++ detector2.js
@@ -1,10 +1,46 @@
+/* Return
+ * @param {Chart} charts
+ * @return {Map}
+ * @example
+ * const chart1 = new Chart('A', 'B', 'C', 'D', 'E');
+ * const chart2 = new Chart('E', 'D', 'C', 'B', 'A');
+ * createDatabase(chart1, chart2);
+ * // Returns {'A' => { history: [1, 5])}, 'B' => { history: [2, 4])}, 'C' => { history: [3, 3])}, 'D' => { history: [4, 2])}, 'E' => { history: [5, 1])}}
+ */
+function runTests(week, charts, detector, tests) {
+  charts = charts.slice();
+  
+  for (const test of tests) {
+    charts.splice(week - 1, 1, ...test.splice(0, 1));
+
+    const [chartA, chartB] = charts.slice(week - 1, week);
+    const database = createDatabase(...charts.before(chartA));
+    const entries = Chart[`detector${detector}`](chartA, chartB, database);
+ 
+    displayTable(...charts.before(chartB), chartB);
+
+    test.forEach((test, index) => {
+
+      if (index === 0) {
+        test(entries);
+      }
+      else {
+//         const entry = entries.shift();
+//         const values = Chart[`corrector${detector}`](entry, chartA, chartB, database);
+//         Chart[`sorter${detector}`](entry, values, [chart1, chart2, chartA, chartB]);
+//         test(values);
+      }
+    });
+  }
+}
+
+
 // WEEK 2
 {
   const tests = [
    // POSITION 1
    [
      new Chart("All Star", "I Need To Know", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Mi Chico Latino", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Higher", "That's The Way It Is", "Man! I Feel Like A Woman", "If Ya Gettin' Down", "Don't Say You Love Me", "Dreams In Digital", "The Kids Aren't Alright", "When You're Gone", "As Fast As You Can"),
-     new Chart("All Star", "Angels", "I Need To Know", "Mi Chico Latino", "Someday We'll Know", "New", "Someday", "That's The Way It Is", "Si Me Advertí", "Puente", "No Quiero Verte", "Man! I Feel Like A Woman", "Don't Say You Love Me", "All I Have To Give", "Dreams In Digital", "All Or Nothing", "Higher", "As Fast As You Can", "If Ya Gettin' Down", "When You're Gone"),
 
      function (entries) {
        expect(entries.length).to.equal(1);
@@ -18,11 +54,11 @@
 
                                                        // [09, 09, 09, 11]  [06, 07, 10, 13]
        expect(entries[1]).to.equal("Look At Me");      // [09, 09, 10, 11]  [06, 07, 09, 13]
-       
-                                                       // [09, 09, 09, 11]  [06, 07, 10, 13]
-       expect(entries[1]).to.equal("Angels");          // [09, 09, 10, 11]  [06, 07, 09, 13]
+ 
      },
    ],
   ];
+
+  runTests(2, charts, 2, tests);
 }
 
