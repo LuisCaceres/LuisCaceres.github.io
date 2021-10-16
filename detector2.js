fd6463dc8681e856afdfcaa9ac27b52976942041
Luis Caceres <luiscaceresmx@gmail.com>
2021-10-16 23:03:21 +1100

    Update detector2.js

--- detector2.js
+++ detector2.js
@@ -101,7 +101,7 @@ function runTests2(week, charts, tests) {
   const tests = [
    // POSITION 8
    [
-     new Chart('All Star', 'Angels', 'Mi Chico Latino', 'I Need To Know', "That's The Way It Is", 'New', "Someday We'll Know", 'Maria Maria', 'Si Me Advertí', 'Someday', 'Fly Away', 'Man! I Feel Like A Woman', 'Puente', 'All Or Nothing', 'No Quiero Verte', 'Super Trouper', 'As Fast As You Can', 'All I Have To Give', "Don't Say You Love Me", 'Dreams In Digital'),
+     new Chart('All Star', 'Angels', 'I Need To Know', 'Mi Chico Latino', "Someday We'll Know", 'New', 'Someday', "That's The Way It Is", 'Si Me Advertí', 'Puente', 'No Quiero Verte', 'Man! I Feel Like A Woman', "Don't Say You Love Me", 'All I Have To Give', 'Dreams In Digital', 'All Or Nothing', 'Higher', 'As Fast As You Can', "If Ya Gettin' Down", "When You're Gone"),
 
      function (entries) {
        expect(entries.length).to.equal(2);
