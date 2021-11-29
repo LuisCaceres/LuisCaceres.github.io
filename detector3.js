2b794681317a85404029b83eb17dbbbb50d042fe
Luis Caceres <luiscaceresmx@gmail.com>
2021-11-29 23:22:02 +1100

    Update detector3.js

--- detector3.js
+++ detector3.js
@@ -1,8 +1,18 @@
 // WEEK 6
 {
   const tests = [
-    
+    // POSITION 1
+    [
+      new Chart("All Star", "I Need To Know", "Someday We'll Know", "Puente", "Angels", "Someday", "All I Have To Give", "Higher", "New", "Si Me Advertí", "If Ya Gettin' Down", "Mi Chico Latino", "No Quiero Verte", "The Kids Aren't Alright", "That's The Way It Is", "La Lola", "When You're Gone", "Man! I Feel Like A Woman", "Don't Say You Love Me", "As Fast As You Can"),
+      new Chart("All Star", "I Need To Know", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Mi Chico Latino", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Higher", "That's The Way It Is", "Man! I Feel Like A Woman", "If Ya Gettin' Down", "Don't Say You Love Me", "Dreams In Digital", "The Kids Aren't Alright", "When You're Gone", "As Fast As You Can"),
+
+      function (entries) {
+        expect(entries.length).to.equal(0);
+      },
+    ],    
   ];
+    
+  runTests(8, charts, 3, tests);
 }
 
 // WEEK 7
