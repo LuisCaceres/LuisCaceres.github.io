27b41dfe8185c780bc4e86349952454ae2b3e461
Luis Caceres <luiscaceresmx@gmail.com>
2021-12-01 17:54:33 +1100

    Update detector3.js

--- detector3.js
+++ detector3.js
@@ -197,6 +197,16 @@ EXAMPLE: [20, 20, 20]
         expect(entries).to.include("All I Have To Give");  // [11, 11, 14, 15]  [07, 10, 11, 18]
       },
     ],
+
+    // POSITION 12
+    [
+      new Chart("All Star", "I Need To Know", "Someday We'll Know", "Puente", "Angels", "Someday", "All I Have To Give", "Higher", "New", "Si Me Advertí", "If Ya Gettin' Down", "Man! I Feel Like A Woman", "Mi Chico Latino", "No Quiero Verte", "The Kids Aren't Alright", "That's The Way It Is", "La Lola", "When You're Gone", "Don't Say You Love Me", "As Fast As You Can"),
+      new Chart("All Star", "I Need To Know", "Angels", "Someday We'll Know", "Puente", "Someday", "New", "Mi Chico Latino", "Si Me Advertí", "All I Have To Give", "No Quiero Verte", "Man! I Feel Like A Woman", "Higher", "That's The Way It Is", "If Ya Gettin' Down", "Don't Say You Love Me", "Dreams In Digital", "The Kids Aren't Alright", "When You're Gone", "As Fast As You Can"),
+
+      function (entries) {
+        expect(entries.length).to.equal(0);
+      },
+    ],
   ];
     
   runTests(8, charts, 3, tests);
