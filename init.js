2bcb951e3bb918ead5ca3a80b29954da34196bcf
Luis Caceres <luiscaceresmx@gmail.com>
2022-01-02 20:34:52 +1100

    Update init.js

--- init.js
+++ init.js
@@ -60,7 +60,7 @@ async function onYouTubeIframeAPIReady() {
 //   list = format(lists.current, lists.next, charted);
  
   const result = associate(currentList, charted, uncharted); /* associate(list, charted, uncharted); */
-//   insertExtraItems(result.chart, result.uncharted);
+  insertExtraItems(result.chart, result.uncharted);
 
   let playlist = result.chart; /* generatePlaylist(result.chart, intro, sting, advertisement, newVideo); */
   playlist = adjustPlaylist(playlist);
