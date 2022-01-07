4f02b4842ddb0cd34854f3db3d4424aa7956d21a
Luis Caceres <luiscaceresmx@gmail.com>
2022-01-08 01:42:44 +1100

    Create test.js

--- test.js
+++ test.js
@@ -324,7 +324,7 @@ function generateList(outcoming, incoming) {
 
   const list = ['Entry 1', 'Entry 3'];
 
-  const { chart } = createChart(list, charted, uncharted);
+//   const { chart } = createChart(list, charted, uncharted);
 
 //   expect(chart.length).to.equal(2);
 //   expect(chart[0]).to.equal('Song C');
