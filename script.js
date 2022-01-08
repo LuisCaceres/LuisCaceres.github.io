fb27a01ae0f81dc17e3ba68851901d0accea3a1e
Luis Caceres <luiscaceresmx@gmail.com>
2022-01-08 16:42:48 +1100

    Update script.js

--- script.js
+++ script.js
@@ -629,7 +629,7 @@ class Chart extends List {
 
     // Find entries in this chart able to eliminate continuity errors. 
     errors.forEach(error => {
-      const targets = Chart.corrector3(error, this, listB, database);
+      const targets = Chart.corrector3(error, this, database);
 //       Chart.sorter3(error, target, [chart1, chart2, this, listB]);
       map.set(error, targets);
     });
