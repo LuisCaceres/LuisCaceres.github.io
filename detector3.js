c378ad4dfe7d8ce6b7688c1819c1fc426104e83c
Luis Caceres <luiscaceresmx@gmail.com>
2022-01-08 15:36:52 +1100

    Update detector3.js

--- detector3.js
+++ detector3.js
@@ -2393,15 +2393,6 @@ EXAMPLE: [20, 20, 20]
        expect(entries).to.include("All I Have To Give");
      },
 
-     function (entries) { // Si Me Advertí
-       expect(entries.length).to.equal(2);
-                                                    // [09, 09, 09, 11]  [**, **, 08, 08]
-       expect(entries[0]).to.equal("Maria Maria");  // [09, 09, 08, 11]  [**, **, 09, 08]
-
-                                                    // [09, 09, 09, 11]  [06, 07, 10, 13]
-       expect(entries[1]).to.equal("Someday");      // [09, 09, 10, 11]  [06, 07, 09, 13]
-     },
-
      function (entries) { // All I Have To Give
        expect(entries.length).to.equal(2);
                                                               // [18, 18, 18, **]  [15, 13, 19, **]
@@ -2409,6 +2400,15 @@ EXAMPLE: [20, 20, 20]
 
                                                               // [18, 18, 18, **]  [16, 14, 20, 20]
        expect(entries[1]).to.equal("Dreams In Digital");      // [18, 18, 20, **]  [16, 14, 18, 20]
+     },
+
+     function (entries) { // Si Me Advertí
+       expect(entries.length).to.equal(2);
+                                                    // [09, 09, 09, 11]  [**, **, 08, 08]
+       expect(entries[0]).to.equal("Maria Maria");  // [09, 09, 08, 11]  [**, **, 09, 08]
+
+                                                    // [09, 09, 09, 11]  [06, 07, 10, 13]
+       expect(entries[1]).to.equal("Someday");      // [09, 09, 10, 11]  [06, 07, 09, 13]
      },
    ],
 
