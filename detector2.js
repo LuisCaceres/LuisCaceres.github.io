9d5d8773b719ddd6a0dd25bf806a66223007ea86
Luis Caceres <luiscaceresmx@gmail.com>
2022-01-22 23:42:09 +1100

    Update detector2.js

--- detector2.js
+++ detector2.js
@@ -39,3 +39,29 @@ function runTests2(week, charts, tests) {
     });
   }
 }
+
+// WEEK 8
+{
+  const tests = [
+
+   // POSITION 12
+   [
+     new Chart("All Star", "Angels", "I Need To Know", "Mi Chico Latino", "Someday We'll Know", "New", "Someday", "That's The Way It Is", "Si Me Advertí", "Puente", "No Quiero Verte", "Tengo Frío", "Man! I Feel Like A Woman", "Don't Say You Love Me", "All I Have To Give", "Dreams In Digital", "All Or Nothing", "Higher", "As Fast As You Can", "If Ya Gettin' Down"),
+     new Chart("All Star", "Angels", "Mi Chico Latino", "I Need To Know", "That's The Way It Is", "New", "Someday We'll Know", "Maria Maria", "Si Me Advertí", "Someday", "Fly Away", "Tengo Frío", "Man! I Feel Like A Woman", "Puente", "All Or Nothing", "No Quiero Verte", "Super Trouper", "As Fast As You Can", "All I Have To Give", "Dreams In Digital"),
+
+     function (entries) {
+       expect(entries.length).to.equal(1);
+       expect(entries).to.include("Tengo Frío");
+     },
+
+     function (entries) { // Tengo Frío
+       expect(entries.length).to.equal(1);
+                                                                // [**, **, 12, 10]  [12, 12, 18, **]
+       expect(entries).to.include("Man! I Feel Like A Woman");  // [**, **, **, **]  [12, 12, 12, 10]
+     },
+   ],
+  ];
+
+  runTests(10, testCharts, 2, tests);
+}
+
