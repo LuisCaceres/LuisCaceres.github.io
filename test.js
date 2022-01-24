e2ea3dab8d09991c4bbb7ae2c1bd5269d1ae23b8
Luis Caceres <luiscaceresmx@gmail.com>
2022-01-24 21:54:15 +1100

    Update test.js

--- test.js
+++ test.js
@@ -335,28 +335,6 @@ function generateList(outcoming, incoming) {
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// at()
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-{
-  const array = [1, 2, 3, 4, 5];
-  
-  expect(array.at(0)).to.equal(1);
-  expect(array.at(1)).to.equal(2);
-  expect(array.at(2)).to.equal(3);
-  expect(array.at(3)).to.equal(4);
-  expect(array.at(4)).to.equal(5);
-  expect(array.at(5)).to.equal(undefined);
-  
-  expect(array.at(-1)).to.equal(5);
-  expect(array.at(-2)).to.equal(4);
-  expect(array.at(-3)).to.equal(3);
-  expect(array.at(-4)).to.equal(2);
-  expect(array.at(-5)).to.equal(1);
-  expect(array.at(-6)).to.equal(undefined);
-}
-
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // generatePlaylist()
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
